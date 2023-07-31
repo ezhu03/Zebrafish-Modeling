d97d157d4e600f257d813b594e9b57dcf9d795be
stephsplash30 <ezhu@ucsb.edu>
2023-07-31 10:46:30 -0700

    Update fish0data.py

--- fish0data.py
+++ fish0data.py
@@ -1,4 +1,7 @@
 from numpy import load
+import numpy as np
+import math
+import matplotlib.pyplot as plt
 
 data = load('2023-07-27_16-08-57.mp4_visual_field_fish0.npz')
 lst = data.files
@@ -7,13 +10,17 @@ for item in lst:
         positions = data[item]
 
     if item == 'fish_angle':
-        directions = data[item]
-print(positions)
+        angles = data[item]
+        directions = np.zeros((len(angles),2))
+        for i in range(len(angles)):
+            directions[i,0]=math.cos(angles[i])
+            directions[i,1]=math.sin(angles[i])
+print(positions[:,1])
 print(directions)
-import numpy as np
-import matplotlib.pyplot as plt
 
-def plot_quiver(positions, directions, scale=1.0, title=None):
+
+
+def plot_quiver(positions, directions, sc=1.0, title=None):
     """
     Plot a quiver plot of positions and directions.
 
@@ -24,14 +31,19 @@ def plot_quiver(positions, directions, scale=1.0, title=None):
         title (str, optional): Title of the plot. Default is None.
     """
     fig, ax = plt.subplots()
-    ax.quiver(positions[:, 0], positions[:, 1], directions[:, 0], directions[:, 1], scale=scale)
-    ax.set_aspect('equal')
-    ax.set_xlabel('X')
-    ax.set_ylabel('Y')
+    for i in range(len(positions)):
+        ax.clear()
+        ax.quiver(positions[i, 0], positions[i, 1], sc*directions[i, 0], sc*directions[i, 1], scale=1)
+        ax.set_aspect('equal')
+        ax.set_xlim((300,1000))
+        ax.set_ylim((0,700))
+        ax.set_xlabel('X')
+        ax.set_ylabel('Y')
+        plt.pause(0.0001)
 
     if title:
         ax.set_title(title)
 
     plt.show()
 
-plot_quiver(positions, directions, scale=1.0, title="Quiver Plot Example")
+plot_quiver(positions, directions, sc=0.05, title="Quiver Plot Example")
