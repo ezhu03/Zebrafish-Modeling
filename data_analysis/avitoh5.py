26ba2f35064136861786865c4cac8c368b797888
stephsplash30 <ezhu@ucsb.edu>
2023-08-07 11:31:13 -0700

    Update avitoh5.py

--- data_analysis/avitoh5.py
+++ data_analysis/avitoh5.py
@@ -5,31 +5,27 @@ import os
 # Replace 'input.avi' with the path to your AVI file
 input_video_path = 'singlefishtest_2023-07-31-152515-0000.avi'
 
-# Initialize VideoCapture object to read frames from the AVI file
-video_capture = cv2.VideoCapture(input_video_path)
-
 # Replace 'output_folder' with the path to the folder where you want to save the HDF5 files
 output_folder = 'data_output/singlefishtest_2023-07-31-152515-0000'
 
-# Create the output folder if it doesn't exist
-if not os.path.exists(output_folder):
-    os.makedirs(output_folder)
+# Replace 'output_file_name.h5' with the desired name for the output HDF5 file
+output_file_name = 'singlefishtest_2023-07-31-152515-0000.h5'
 
-# Loop through the video and write each frame to a separate HDF5 file
-frame_count = 0
-while True:
-    ret, frame = video_capture.read()
-    if not ret:
-        break
+# Initialize VideoCapture object to read frames from the AVI file
+video_capture = cv2.VideoCapture(input_video_path)
 
-    frame_count += 1
+# Create the HDF5 file
+with h5py.File(output_file_name, 'w') as h5file:
+    frame_count = 0
+    while True:
+        ret, frame = video_capture.read()
+        if not ret:
+            break
 
-    # Replace 'output_file_prefix' with the desired prefix for the HDF5 file names
-    output_file_name = os.path.join(output_folder, f'output_file_prefix_{frame_count:04d}.h5')
+        # Create a dataset within the HDF5 file to store the frame
+        h5file.create_dataset(f'/frame_{frame_count:04d}', data=frame)
 
-    # Write the frame data to the HDF5 file
-    with h5py.File(output_file_name, 'w') as h5file:
-        h5file.create_dataset('/frame', data=frame)
+        frame_count += 1
 
 # Release the VideoCapture object
 video_capture.release()
