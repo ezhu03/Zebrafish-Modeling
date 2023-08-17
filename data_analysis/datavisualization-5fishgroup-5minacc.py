72fc58556bde8ee0d6283cef49c50a1309f1d3f9
stephsplash30 <ezhu@ucsb.edu>
2023-08-17 15:50:25 -0700

    updates to 2 files

--- data_analysis/datavisualization-5fishgroup-5minacc.py
+++ data_analysis/datavisualization-5fishgroup-5minacc.py
@@ -2,7 +2,6 @@ from numpy import load
 import numpy as np
 import math
 import matplotlib.pyplot as plt
-from matplotlib.patches import Circle
 file0 = 'D:\\output\\5fish_group\\5fish_group_maxbright_5minacc_2023-08-09-155141-0000_fish0.npz'
 file1 = 'D:\\output\\5fish_group\\5fish_group_maxbright_5minacc_2023-08-09-155141-0000_fish1.npz'
 file2 = 'D:\\output\\5fish_group\\5fish_group_maxbright_5minacc_2023-08-09-155141-0000_fish2.npz'
@@ -48,40 +47,4 @@ positions3, directions3 = open_file(file3)
 positions4, directions4 = open_file(file4)
 positions=[positions0, positions1, positions2, positions3, positions4]
 directions=[directions0, directions1, directions2, directions3, directions4]
-
-
-
-def plot_quiver(positions, directions, sc=1.0, ratio=1.0, title=None):
-    """
-    Plot a quiver plot of positions and directions.
-
-    Parameters:
-        positions (numpy.ndarray): An array of shape (N, 2) containing x and y positions.
-        directions (numpy.ndarray): An array of shape (N, 2) containing x and y directions.
-        scale (float, optional): Scaling factor for the arrow length. Default is 1.0.
-        title (str, optional): Title of the plot. Default is None.
-    """
-    fig, ax = plt.subplots()
-    for i in range(len(positions[0])):
-        ax.clear()
-        circle = Circle([length/2,length/2], length/2, edgecolor='b', facecolor='none')
-        plt.gca().add_patch(circle)
-        ax.quiver(ratio*positions[0][i, 0], ratio*positions[0][i, 1], sc*directions[0][i, 0], sc*directions[0][i, 1], scale=2)
-        ax.quiver(ratio*positions[1][i,0], ratio*positions[1][i, 1], sc*directions[1][i, 0], sc*directions[1][i, 1], scale=2)
-        ax.quiver(ratio*positions[2][i, 0], ratio*positions[2][i, 1], sc*directions[2][i, 0], sc*directions[2][i, 1], scale=2)
-        ax.quiver(ratio*positions[3][i, 0], ratio*positions[3][i, 1], sc*directions[3][i, 0], sc*directions[3][i, 1], scale=2)
-        ax.quiver(ratio*positions[4][i, 0], ratio*positions[4][i, 1], sc*directions[4][i, 0], sc*directions[4][i, 1], scale=2)
-
-        ax.set_aspect('equal')
-        ax.set_xlim((0,length))
-        ax.set_ylim((0,length))
-        ax.set_xlabel('X')
-        ax.set_ylabel('Y')
-        if title:
-            ax.set_title(title)
-        plt.pause(0.0001)
-        
-
-    plt.show()
-len_ratio = length/30
-plot_quiver(positions, directions, sc=0.1, ratio = len_ratio, title="Quiver Plot Example")
+print(np.flatten(positions))
