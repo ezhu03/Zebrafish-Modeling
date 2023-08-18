73ea78a4b4e6d06a8a05d0bc3952588fdf6f9bcc
stephsplash30 <ezhu@ucsb.edu>
2023-08-17 18:48:17 -0700

    Update datavisualization-5fishsingle-5minacc.py

--- data_analysis/datavisualization-5fishsingle-5minacc.py
+++ data_analysis/datavisualization-5fishsingle-5minacc.py
@@ -2,11 +2,11 @@ from numpy import load
 import numpy as np
 import math
 import matplotlib.pyplot as plt
-file0 = 'D:\\output\\5fish_group\\5fish_group_maxbright_5minacc_2023-08-09-155141-0000_fish0.npz'
-file1 = 'D:\\output\\5fish_group\\5fish_group_maxbright_5minacc_2023-08-09-155141-0000_fish1.npz'
-file2 = 'D:\\output\\5fish_group\\5fish_group_maxbright_5minacc_2023-08-09-155141-0000_fish2.npz'
-file3 = 'D:\\output\\5fish_group\\5fish_group_maxbright_5minacc_2023-08-09-155141-0000_fish3.npz'
-file4 = 'D:\\output\\5fish_group\\5fish_group_maxbright_5minacc_2023-08-09-155141-0000_fish4.npz'
+file0 = 'D:\\output\\5fish_single\\5fish_single_maxbright_5minacc_2023-08-09-153535-0000_fish0.npz'
+file1 = 'D:\\output\\5fish_single\\5fish_single_maxbright_5minacc_2023-08-09-153535-0000_fish1.npz'
+file2 = 'D:\\output\\5fish_single\\5fish_single_maxbright_5minacc_2023-08-09-153535-0000_fish2.npz'
+file3 = 'D:\\output\\5fish_single\\5fish_single_maxbright_5minacc_2023-08-09-153535-0000_fish3.npz'
+file4 = 'D:\\output\\5fish_single\\5fish_single_maxbright_5minacc_2023-08-09-153535-0000_fish4.npz'
 global length
 length = 9
 def open_file(file_name):
@@ -54,10 +54,17 @@ for i in range(len(positions0)):
     positions.append(positions4[i])
     directions.append(directions4[i])
 distances=[]
+total_distance=0
+count = 0
 for position in positions:
-    distance = math.sqrt((position[0]-length/2)**2 + (position[1]-length/2)**2)
+    distance = math.sqrt((length*(position[0]/30-1/2))**2 + (length*(position[1]/30-1/2))**2)
+    if distance < 100:
+        total_distance += distance
+        count +=1
     distances.append(distance)
-plt.hist(distances,bins=[0,0.5,1,1.5,2,2.5,3,3.5,4,4.5, 5])
+plt.hist(distances,bins=[0,0.5,1,1.5,2,2.5,3,3.5,4,4.5, 5,10])
+avgdistance = total_distance / count
+plt.title("avg distance = " + str(avgdistance))
 plt.show()
 speeds=[]
 for direction in directions:
